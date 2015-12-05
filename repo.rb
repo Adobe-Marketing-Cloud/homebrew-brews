6d945526caf9736a5ebaab0d7e363b6c398de2b8
Alexander Klimetschek <aklimets@adobe.com>
2015-12-04 16:52:21 -0800

    removing unnecessary dependencies curl & rsync (come with OSX) from repo formula

--- repo.rb
+++ repo.rb
@@ -6,9 +6,6 @@ class Repo < Formula
   sha1 "ea4f3e5404d81d9fb2c4d7e1804b128426fcd2a1"
   version "1.2"
 
-  depends_on "curl"
-  depends_on "rsync"
-
   conflicts_with "repo", :because => "Android's repo also provides a 'repo' binary/script"
 
   def install
