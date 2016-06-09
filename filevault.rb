bf578e9f5e004a66781921d1d55acb3de6f33233
Roland Schaer <rschaer@adobe.com>
2016-06-09 12:00:36 +0200

    updating filevault to 3.1.26

--- filevault.rb
+++ filevault.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Filevault < Formula
   homepage 'http://dev.day.com/docs/en/crx/current/how_to/how_to_use_the_vlttool.html'
-  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.18/vault-cli-3.1.18-bin.tar.gz'
-  sha1 'b165b53072559dac79de58464c64710286fa1e09'
-  version '3.1.18'
+  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.26/vault-cli-3.1.26-bin.tar.gz'
+  sha1 '1d0ace83b8ec0fcee9929e8b683470f7459cf8f3'
+  version '3.1.26'
 
   def install
     # Remove windows files
