d9278bd8d3d77b5a4a183cbc294c19c7fc24f0b9
justinedelson <justin@justinedelson.com>
2016-09-13 11:39:07 -0400

    using sha256 for filevault

--- filevault.rb
+++ filevault.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Filevault < Formula
   homepage 'http://dev.day.com/docs/en/crx/current/how_to/how_to_use_the_vlttool.html'
   url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.28/vault-cli-3.1.28-bin.tar.gz'
-  sha1 'd9f5dcb6e5877331c8348fb2a380f67deb7c44b5'
+  sha256 '3687b27e729e9ed4ba3533d0c563f80b84d8ecb5cdf9ed2f1ceb559478d053ca'
   version '3.1.28'
 
   def install
