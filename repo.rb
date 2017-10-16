393af7bfaa30ff5b188f98c102a4df3ef3574161
Alexander Klimetschek <aklimets@adobe.com>
2017-10-16 14:41:30 -0700

    updating repo to 1.4

--- repo.rb
+++ repo.rb
@@ -1,9 +1,8 @@
 class Repo < Formula
   desc "FTP-like tool for JCR content"
   homepage "https://github.com/Adobe-Marketing-Cloud/tools/tree/master/repo"
-  url "https://github.com/Adobe-Marketing-Cloud/tools/releases/download/repo-v1.3/repo"
-  version "1.3"
-  sha256 "9f8dc24a7bccaf72bad409b66b165b0eaf9d02feb24bd1f7a1f9df493c0f9fcc"
+  url "https://github.com/Adobe-Marketing-Cloud/tools/releases/download/repo-1.4/repo"
+  sha256 "4c627df9ee17f62838161e0525bb42349c01787b90f4365d836641c9a3ea16ce"
 
   conflicts_with "repo", :because => "Android's repo also provides a 'repo' binary/script"
 
