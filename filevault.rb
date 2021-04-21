6ada5054d8c739e56c6f5ad4f49f16ebb16be4f7
AzizTheSmart <82464819+AzizTheSmart@users.noreply.github.com>
2021-04-21 15:19:27 +0530

    Update http's' in filevault

--- filevault.rb
+++ filevault.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Filevault < Formula
   homepage 'http://jackrabbit.apache.org/filevault/overview.html'
-  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.44/vault-cli-3.1.44-bin.tar.gz'
+  url 'https://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.44/vault-cli-3.1.44-bin.tar.gz'
   sha256 'd3878a59b7c25f89e4f3701abf2892702ea435b4f07633065faa7a6fbf9fb36e'
   version '3.1.44'
 
